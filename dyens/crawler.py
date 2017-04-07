a23868d2f35aea7f56efedb2d4ad12534543a9ae
Julian Ailan <jtulians@gmail.com>
2017-04-06 21:09:37 -0300

    Added data dumping file along with its associated path (1st iteration, gotta lighten it up). Pypi set.

--- dyens/crawler.py
+++ dyens/crawler.py
@@ -3,29 +3,50 @@
 # ===================================
 
 # Built-in modules
+import re
+import sys
+import json
 import time
 import argparse
 from urllib.parse import urljoin
 from urllib.error import URLError
 from urllib.request import urlopen
 from html.parser import HTMLParser
+from collections import defaultdict
 from http.client import BadStatusLine
 
 
 parser = argparse.ArgumentParser(prog="dyens", description="Dyens CLI")
 parser.add_argument("start_url", help="Base URL to start crawling from.")
+parser.add_argument("path", help="Document in which to save data.")
 parser.add_argument("words", nargs="*", help="Words to look for.")
 args = parser.parse_args()
 
 
-class LinksGetter(HTMLParser):
+class LinkContentParser(HTMLParser):
     """Returns an HTML file along with the URLs in it."""
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.sites_content = {}
+
     def handle_starttag(self, tag, attrs):
         if tag == "a":
             for (key, value) in attrs:
                 if key == "href":
                     new_url = urljoin(self.base_url, value)
                     self.links += [new_url]
+
+    def handle_data(self, tag):
+        # Work only with non-empty tag strings.
+        if re.sub("[\s+]", "", tag):
+            if self.base_url not in self.sites_content:
+                self.sites_content[self.base_url] = tag
+            else:
+                self.sites_content[self.base_url] += " " + tag
+            if sys.getsizeof(self.sites_content) > 10e6:
+                dump(self.sites_content, args.path)
+                self.sites_content = None
 
     def get_links(self, url):
         self.links = []
@@ -40,29 +61,34 @@ class LinksGetter(HTMLParser):
                 html_string = html_bytes.decode("utf-8")
             except UnicodeDecodeError:
                 html_string = html_bytes.decode("latin-1")
+            # Feed the HTML Parser with data to be parsed by handling functs.
             self.feed(html_string)
             return (html_string, self.links)
         else:
             return ("", [])
 
 
-def crawler(words, start_url, max_pages=1000000):
+def crawler(words, start_url, max_pages=10000, display=True):
     """Performs web crawling, returning the site that holds the words."""
     pages_to_visit, number_visited, previous_time = [start_url], 0, 0
-    pages_seen, parser = set(pages_to_visit), LinksGetter()
+    pages_seen, parser = set(pages_to_visit), LinkContentParser()
     search_start_time = time.time()
     while number_visited < max_pages and pages_to_visit:
         current_url = pages_to_visit[0]
         pages_to_visit = pages_to_visit[1:]
+        # Measure how much time it takes to get page links.
         previous_time = time.time()
         data, links = parser.get_links(current_url)
         # Process elapsed time.
         previous_time = time.time() - previous_time
         minutes, seconds = divmod((time.time() - search_start_time), 60)
         hours, minutes = divmod(minutes, 60)
-        print("{:02.0f}:{:02.0f}:{:02.0f}".format(hours, minutes, seconds),
-              "| #", number_visited, "- Visited:", current_url, "in",
-              "{0:.5f}".format(previous_time), "seconds.")
+        if display:
+            print("{:02.0f}:{:02.0f}:{:02.0f}".format(hours, minutes, seconds),
+                  "| #", number_visited, "- Visited:", current_url, "in",
+                  "{0:.5f}".format(previous_time), "seconds. Set size:",
+                  sys.getsizeof(pages_seen), "bytes.", len(pages_to_visit),
+                  "sites pending to visit.")
         number_visited += 1
         check_if_contained = [data.find(word) for word in words]
         if all(check > -1 for check in check_if_contained):
@@ -77,6 +103,17 @@ def crawler(words, start_url, max_pages=1000000):
         print("SUCCESS:", words, "found at", current_url)
     else:
         print("FAILED: Words never found.")
+
+
+def dump(data, path):
+    with open(path, "w+") as f:
+        d = json.load(f)
+        # BUG: when same key exists in both dicts.
+        d = {**d, **data}
+        f.write(json.dumps(d,
+                           sort_keys=True,
+                           indent=4,
+                           separators=(",", ": ")))
 
 
 if __name__ == '__main__':
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name="dyens",
+    packages=["dyens"],
+    version="0.0.1",
+    description="Web crawler inspired by Roland Dyens' hands.",
+    author="Julian Ailan",
+    author_email="jtulians@gmail.com",
+    url="https://github.com/tulians/dyens",
+    download_url="https://github.com/tulians/dyens/archive/0.0.1.tar.gz",
+    keywords=["crawler", "webcrawler"],
+    classifiers=[],
+)
